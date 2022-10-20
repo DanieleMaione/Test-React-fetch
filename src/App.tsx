b1bdacaa24c57a763abd747bb15ab5df334c2099
danielemaione-bitrocket <macbook-bitrocket.dev@MacBook-Pro-di-macbook-bitrocketdev.local>
2022-10-20 18:07:43 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import userEvent from "@testing-library/user-event";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+import { Post } from "./components/Post";
 
-function App() {
+type getPlaceHolderObjectType = {
+  userid: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const App = () => {
+  const [posts, setPosts] = useState<Array<getPlaceHolderObjectType>>([]);
+  const [Identify, getIdentify] = useState<number>();
+  const [active, setActive] = useState("HomePage");
+  const [opened, setOpened] = useState(false);
+  const [openedPost, setOpenedPost] = useState<getPlaceHolderObjectType>();
+
+  const getPlaceHolderAPI = async () => {
+    const result = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await result.json();
+    setPosts(data);
+  };
+
+  const getBody = async (id: number) => {
+    const result = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}`
+    );
+    const ident = await result.json();
+    getIdentify(ident);
+  };
+
+  useEffect(() => {
+    getPlaceHolderAPI();
+  }, []);
+
+  console.log(posts);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts.map((elements) => {
+        return (
+          <div>
+            <button onClick={() => setActive("HomePage")}>HomePage</button>
+            <button onClick={() => setActive("SecondPage")}>BodyPage</button>
+            {active === "HomePage" && `${elements.id}-${elements.title}`}
+            {active === "SecondPage" && `${elements.body}`}
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
